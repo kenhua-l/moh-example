3ca8e43e872c39a5d2f1d2a4523f33f7f3f9001c
kenhua-l <l_kenhua93@hotmail.com>
2023-04-05 17:22:17 +0800

    update announcements, button links and video tab

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,38 @@
-(function() {
+$(function() {
   // everything on document ready
+  
+  // tab
+  $(".tab-container .tab-list .tab-list-item").on("click", function () {
+    var dataTab = $(this).data('tab');
+    var tabContainer = $(this).closest('.tab-container');
+
+    tabContainer.find('.tab-content-item').attr('aria-hidden', true);
+    tabContainer.find('.tab-content-item[data-tab='+dataTab+']').attr('aria-hidden', false);
+
+    $(this).siblings('.tab-list-item').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  $(".tab-container .tab-list .tab-list-item:first-child").click();
+
+  $('.video').on('click', function() {
+    $('.video').removeClass('focus');
+    $(this).addClass('focus');
+  });
+  $(document).on('click', function(e) {
+    if(!$(e.target).closest('.video').length) {
+      $('.video').removeClass('focus');
+    }
+  });
+  
+  $('.video-tab .tab-list-item').on('click', function() {
+    $('.embed-responsive-item').each(function () {
+      $(this)[0].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+    })
+  });
 
 });
+
+$(window).on('resize orientationchange load', function(e) {
+
+}).resize();
